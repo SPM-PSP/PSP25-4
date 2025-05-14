89873bee291e6387eafab1c54948f47d55356b18
appleapple6699 <2647986519@qq.com>
2025-05-14 17:14:33 +0800

    upgrade week 11

--- src/deepseek.py
+++ src/deepseek.py
@@ -1,19 +1,20 @@
 import os
 from openai import OpenAI
 
-# 获取环境变量的值
 deepseek_api_key = os.environ.get("DeepSeek API Key")
 
 if deepseek_api_key is None:
     raise ValueError("环境变量 'DeepSeek_API_Key' 未设置")
 
-client = OpenAI(api_key=deepseek_api_key, base_url="https://api.deepseek.com")
-
-messages = [{"role": "user", "content": "What's the highest mountain in the world?"}]
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=messages
+client = OpenAI(
+    api_key=deepseek_api_key,
+    base_url="https://api.deepseek.com/beta",
 )
 
-messages.append(response.choices[0].message)
-print(f"Messages Round 1: {messages}")
+response = client.completions.create(
+    model="deepseek-chat",
+    prompt="春天来了，万物复苏，",
+    suffix="鸟儿在枝头欢快地歌唱，花儿也竞相开放。",
+    max_tokens=128
+)
+print(response.choices[0].text)
